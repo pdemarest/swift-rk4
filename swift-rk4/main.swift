b285884879c17b052c29325a370911d8a6cea12e
Peter Demarest <peter@pdemarest.net>
2014-06-12 20:45:27 -0500

    Added RK4step function

--- swift-rk4/main.swift
+++ swift-rk4/main.swift
@@ -10,3 +10,18 @@ import Foundation
 
 println("Hello, World!")
 
+var y = [1.0]
+
+func deriv (y: Double[], t: Double) -> Double {
+    return y[0]
+}
+
+let f = [deriv]
+var t = 0.0
+let h = 0.1
+
+while t < 2.0 {
+    y = RK4step(y, f, t, h)
+    println(y[0])
+    t += h
+}
