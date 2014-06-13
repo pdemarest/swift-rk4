87a2c809154c1bff98fa4bc5f27dda5b9dfcc48d
Peter Demarest <peter@pdemarest.net>
2014-06-12 21:10:32 -0500

    Set up tests Test y' = y

--- swift-rk4/main.swift
+++ swift-rk4/main.swift
@@ -5,23 +5,38 @@
 //  Created by Peter Demarest on 6/12/14.
 //  Copyright (c) 2014 Peter Demarest. All rights reserved.
 //
-
 import Foundation
 
-println("Hello, World!")
+func test_exp(){
+    // Integrate: y' = y
+    // y_0 = 1.0
+    // from 0 to 2.0
+    
+    var y = [1.0]
+    
+    func deriv (y: Double[], t: Double) -> Double {
+        return y[0]
+    }
+    
+    let f = [deriv]
+    var t = 0.0
+    let h = 0.1
+    
+    
+    while t < 2.0 {
+        y = RK4step(y, f, t, h)
+        t += h
+    }
 
-var y = [1.0]
+    let exact = exp(2.0)
+    let error = y[0] - exact
+    
+    assert(error < pow(h, 4.0))
+    println("y: \(y[0]) exact: \(exact) error: \(error)")
 
-func deriv (y: Double[], t: Double) -> Double {
-    return y[0]
 }
 
-let f = [deriv]
-var t = 0.0
-let h = 0.1
 
-while t < 2.0 {
-    y = RK4step(y, f, t, h)
-    println(y[0])
-    t += h
-}
+println("testing...")
+
+test_exp()
